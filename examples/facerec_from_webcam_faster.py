22620fba817925ec463bb3a1e72371b533d2b4f4
albert andika <albertandika93@gmail.com>
2019-05-24 09:39:10 +0700

    change

--- examples/facerec_from_webcam_faster.py
+++ examples/facerec_from_webcam_faster.py
@@ -22,7 +22,7 @@ video_capture = cv2.VideoCapture(0)
 
 
 # connect to mysql mariadb
-mariadb_connection = mariadb.connect(user='root', password='', database='face_recognition')
+mariadb_connection = mariadb.connect(user='root', password='', database='face_recognation')
 cursor = mariadb_connection.cursor()
 
 #retrieving information
